6a4771ac8c9cd0fa4e9868a7564ef1bbb97fbd1e
João Vitor <joaovitorfelixcontato@gmail.com>
2024-07-23 21:10:59 -0300

    test: add factorial

--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,4 +1,4 @@
-import { fizzBuzz, max } from "../src/intro"
+import { factorial, fizzBuzz, max } from "../src/intro"
 import { expect } from "vitest"
 
 describe('max', () => {
@@ -40,3 +40,29 @@ describe('fizzBuzz', () => {
     expect(fizzBuzz(2)).toBe('2')
   })
 })
+
+describe('factorial', () => {
+  it('should return 1 if given 0', () => {
+    expect(factorial(0)).toBe(1);
+  });
+
+  it('should return 1 if given 1', () => {
+    expect(factorial(1)).toBe(1);
+  });
+
+  it('should return 2 if given 2', () => {
+    expect(factorial(2)).toBe(2);
+  });
+
+  it('should return 6 if given 3', () => {
+    expect(factorial(3)).toBe(6);
+  });
+
+  it('should return 24 if given 4', () => {
+    expect(factorial(4)).toBe(24);
+  });
+
+  it('should return undefined if given a negative number', () => {
+    expect(factorial(-1)).toBeUndefined();
+  });
+});
