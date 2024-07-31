5245941ba9939b4dbdaf671ea999fa0fec199df8
João Vitor <joaovitorfelixcontato@gmail.com>
2024-07-31 09:18:00 -0300

    test: getCoupons and validateUserInput

---
+++ tests/core.test.js
@@ -0,0 +1,71 @@
+/// <reference types="vitest/globals" />
+
+import { getCoupons, validateUserInput } from "../src/core"
+
+describe('getCoupons', () => {
+  it('should be a not empty array', () => {
+    const coupons = getCoupons()
+    expect(Array.isArray(coupons)).toBe(true)
+    expect(coupons.length).toBeGreaterThan(0)
+  })
+
+  it('should return a code as a string', () => {
+    const coupons = getCoupons()
+    coupons.forEach((coupon) => {
+      expect(typeof coupon.code).toBe('string')
+      expect(coupon.code).toBeTruthy()
+    })
+  })
+
+  it('should have discount property as a number between 0 and 1', () => {
+    const coupons = getCoupons()
+    coupons.forEach((coupon) => {
+      expect(typeof coupon.discount).toBe('number')
+      expect(coupon.discount).lessThanOrEqual(1)
+      expect(coupon.discount).greaterThanOrEqual(0)
+    })
+  })
+})
+
+describe('validateUserInput', () => {
+  it('should return an error if username is not a string', () => {
+    const result = validateUserInput(10, 20)
+    expect(result).toMatch(/invalid/i)
+  })
+
+  it('should return an error if username is less than 3 characters', () => {
+    const result = validateUserInput('kk', 20)
+    expect(result).toMatch(/invalid/i)
+  })
+
+  it('should return an error if username is more than 25 characters', () => {
+    const result = validateUserInput('kk'.repeat(26), 20)
+    expect(result).toMatch(/invalid/i)
+  })
+
+  it('should return an error if age is not a number', () => {
+    const result = validateUserInput('caio', '19')
+    expect(result).toMatch(/invalid/i)
+  })
+  
+  it('should return an error if age is lower than 18', () => {
+    const result = validateUserInput('caio', 17)
+    expect(result).toMatch(/invalid/i)
+  })
+
+  it('should return an error if age is higher than 75', () => {
+    const result = validateUserInput('caio', 76)
+    expect(result).toMatch(/invalid/i)
+  })
+
+  it('should return an error if both parameters are invalid', () => {
+    const result = validateUserInput('', 0)
+    expect(result).toMatch(/invalid username/i)
+    expect(result).toMatch(/invalid age/i)
+  })
+
+  it('should return a success message if all parameters are correct', () => {
+    const result = validateUserInput('caio', 20)
+    expect(result).toMatch(/success/i)
+  })
+})
