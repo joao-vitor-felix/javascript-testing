f8c0806b9e74aa047e6ad1e8a7fe678418ba04cf
João Vitor <joaovitorfelixcontato@gmail.com>
2024-08-06 08:03:54 -0300

    test: submitOrder

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -3,9 +3,11 @@
 import {getShippingQuote} from '../src/libs/shipping'
 
 import { vi } from "vitest"
-import { getShippingInfo } from '../src/mocking';
+import { getShippingInfo, submitOrder } from '../src/mocking';
+import { charge } from '../src/libs/payment';
 
 vi.mock('../src/libs/shipping');
+vi.mock('../src/libs/payment');
 
 describe('sendText', () => {
   it('sendText', () => {
@@ -17,22 +19,46 @@ describe('sendText', () => {
 })
 
 describe('getShippingInfo', () => {
-it('should return unavailable message when fetching failed', () => {
-  vi.mocked(getShippingQuote).mockReturnValue('');
+  it('should return unavailable message when fetching failed', () => {
+    vi.mocked(getShippingQuote).mockReturnValue('');
 
-  const result = getShippingInfo('US');
-  expect(result).toMatch(/unavailable/i)
+    const result = getShippingInfo('US');
+    expect(result).toMatch(/unavailable/i)
+  })
+
+  it('should return unavailable message when fetching failed', () => {
+    vi.mocked(getShippingQuote).mockReturnValue({
+      cost: 300,
+      estimatedDays: 5
+    });
+
+    const result = getShippingInfo('US');
+    // expect(result).toMatch('$300')
+    // expect(result).toMatch(/5 days/i)
+    expect(result).toMatch(/shipping cost: \$300 \(5 days\)/i)
+  })
 })
 
-it('should return unavailable message when fetching failed', () => {
-  vi.mocked(getShippingQuote).mockReturnValue({
-    cost: 300,
-    estimatedDays: 5
-  });
+describe('submitOrder', () => {
+  const order = { totalAmount: 100 }
+  const creditCard = { creditCardNumber: 5009 }
+  
+  it('should call charge with correct arguments', async () => {
+    vi.mocked(charge).mockResolvedValue({ status: 'success' });
+    await submitOrder(order, creditCard);
+    expect(charge).toHaveBeenCalledWith(creditCard, order.totalAmount)
+  })
 
-  const result = getShippingInfo('US');
-  // expect(result).toMatch('$300')
-  // expect(result).toMatch(/5 days/i)
-  expect(result).toMatch(/shipping cost: \$300 \(5 days\)/i)
-})
+  it('should receive status as true from submitOrder', async () => {
+    vi.mocked(charge).mockResolvedValue({ status: 'success' });
+    const result = await submitOrder(order, creditCard);
+    expect(result.success).toBe(true)
+  })
+
+  it('should receive status as false from submitOrder', async () => {
+    vi.mocked(charge).mockResolvedValue({ status: 'failed' });
+    const result = await submitOrder(order, creditCard);
+    expect(result.success).toBe(false)
+    expect(result.error).match(/error/i)
+  })
 })
